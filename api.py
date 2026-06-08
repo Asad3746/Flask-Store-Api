de7c873e1227a6aeba33e2e7e72ee79621438fb5
Asad3746 <asad.sajid2003@gmail.com>
2026-06-08 20:26:22 +0500

    fix: bandit

--- api.py
+++ api.py
@@ -46,6 +46,6 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
 
 # writing this to trigger a commit.
